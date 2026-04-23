d3f90354c8b0b6ee4b85cbab1fa51e17bd4c48dd
Gabriela Angel <gabrielaangel2003@hotmail.com>
2026-04-22 21:59:10 -0300

    create function for part 3 and gets variables from user

--- src/main.c
+++ src/main.c
@@ -1,5 +1,36 @@
 #include<stdio.h>
 
+void parte_3(void) {
+/* 
+O sistema deverá conter uma opção para calcular a derivada de uma função simples do tipo:
+f(x) = ax² + bx + c
+O programa deverá receber os coeficientes:
+a
+b
+c
+E apresentar a derivada correspondente:
+f'(x) = 2ax + b
+Exemplo
+Entrada:
+a = 2
+b = 3
+c = 1
+Saída:
+f'(x) = 4x + 3
+*/
+
+int a, b, c;
+
+printf("a: \n");
+scanf("%d", &a);
+
+printf("b: \n");
+scanf("%d", &a);
+
+printf("c: \n");
+scanf("%d", &a);
+}
+
 int main() {
     
 
