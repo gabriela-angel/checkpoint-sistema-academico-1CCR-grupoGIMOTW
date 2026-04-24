329b8eb4f569369e8acc4db71f780e2bda1e83a0
Gabriela Angel Silva <90701152+gabriela-angel@users.noreply.github.com>
2026-04-23 22:50:58 -0300

    Fix erros on main.c

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
+
 float nota1 = -1;
 float nota2 = -1;
 float media = -1;
 int situacao = -1;
+int nova_nota_inserida = 0;
 
 void calculo_media(void){
-
-    int nota1, nota2, media; // Essas são as variáveis utilizadas para o cálculo da média
+    if (nota1 == -1 || nota2 == -1) {
+        printf("NOTAS NAO INSERIDAS\n");
+        return;
+    }
 
     media = (nota1 + nota2) / 2; // Para calcular a média, as notas são somadas e divididas por 2
 }
@@ -24,85 +28,105 @@ void calcula_derivada(void) {
     scanf("%d", &c);
     
     if (b >= 0)
-        print("f'(x) = %dx + b", (2 * a), b);
+        printf("f'(x) = %dx + %d", (2 * a), b);
     else
-        print("f'(x) = %dx - b", (2 * a), -b);
+        printf("f'(x) = %dx - %d", (2 * a), -b);
 }
+
 int verificar_situacao() {
-    float media;
-
-    printf("Digite a media do aluno: ");
-    scanf("%f", &media);
-
+    if (media == -1 || nova_nota_inserida) {
+        calculo_media();
+        nova_nota_inserida = 0;
+    }
     if (media >= 6) {
         printf("APROVADO\n");
         return 0;
-    } else if (media >= 5) {
-        printf("RECUPERACAO\n");
-        return 1;
-    } else {
-        printf("REPROVADO\n");
-        return 2;
     }
-    return 2;
+    else if (media >= 0) {
+        printf("REPROVADO\n");
+        return 1;
+    }
+    return -1;
 }
-void exibir_resultado() {
+
+void inserir_notas() {
+    printf("Nota 1: ");
+    scanf("%f", &nota1);
+    
+    printf("Nota 2: ");
+    scanf("%f", &nota2);
+    nova_nota_inserida = 1;
+}
+
+void exibir_resultado() {    
     printf("\n===== RESULTADO =====\n");
+    
+    if (nota1 == -1 || nota2 == -1)
+        inserir_notas();
     printf("Nota 1: %.2f\n", nota1);
     printf("Nota 2: %.2f\n", nota2);
+
+    if (media == -1 || nova_nota_inserida)
+        calculo_media();
     printf("Media: %.2f\n", media);
+   
     printf("Situacao: ");
-    if (situacao == 0)
-            printf("APROVADO\n");
-   else if (situacao == 1) {
-        printf("RECUPERACAO\n");
-    else {
+    if (situacao == -1 || nova_nota_inserida) {
+        situacao = verificar_situacao();
+        nova_nota_inserida = 0;
+    }
+    else if (situacao == 0)
+        printf("APROVADO\n");
+    else
         printf("REPROVADO\n");
 }
+
 int main() {
     
     int opcao;
-    
-    printf("========================= \n");
-    printf("= SISTEMA EQUIPE GIMOTW =\n");
-    printf("=========================\n");
 
     while(opcao != 6){
+        printf("\n========================= \n");
+        printf("= SISTEMA EQUIPE GIMOTW =\n");
+        printf("=========================\n");
         printf("1 - INSERIR NOTAS\n2 - CALCULAR MEDIAS\n3 - VERIFICAR SITUACAO\n4 - EXIBIR RESULTADO\n5 - CALCULAR DERIVADA\n6 - SAIR\nDigite a opcao desejada: ");
-        
+
         if (scanf("%d", &opcao) != 1) {
-        printf("OPCAO INVALIDA\n");
-        while(getchar() != '\n'); 
-        continue;
+            printf("OPCAO INVALIDA\n");
+            while(getchar() != '\n')
+                continue;
+            continue;
         }
 
         switch(opcao){
             case 1: // INSERIR NOTAS
-            break;
+                inserir_notas();
+                break;
 
             case 2: // CALCULAR MEDIAS
-            break;
+                calculo_media();
+                if (media != -1)
+                    printf("Média: %.2f", media);
+                break;
 
             case 3:  
                 verificar_situacao();
-            break;
+                break;
 
             case 4: 
                 exibir_resultado();
-            break;
+                break;
 
             case 5: // CALCULAR DERIVADA
                 calcula_derivada();
-            break;
+                break;
 
             case 6:
-            printf("FINALIZANDO...\n");
-            break;
+                printf("FINALIZANDO...\n");
+                break;
 
             default:
-            printf("OPCAO INVALIDA\n");
-            break;
-
+                printf("OPCAO INVALIDA\n");
         }
     }
     return 0;
